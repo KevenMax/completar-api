f7fd020354eb29bf0c2ae896ba5e5355de533944
Keven <kmax136@gmail.com>
2019-11-22 23:20:05 -0300

    fix: removing serializers

--- app/serializers/atividade_serializer.rb
+++
@@ -1,4 +0,0 @@
-class AtividadeSerializer < ActiveModel::Serializer
-  attributes :id, :nome, :ativo
-  has_one :categoria
-end
--- app/serializers/categoria_serializer.rb
+++
@@ -1,4 +0,0 @@
-class CategoriaSerializer < ActiveModel::Serializer
-  attributes :id, :nome, :limite_carga_horaria, :ativo
-  has_one :curso
-end
