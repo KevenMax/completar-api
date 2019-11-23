eed1011c62ee3b7515d4964e468a9190d322fefd
Keven <kmax136@gmail.com>
2019-11-22 23:13:20 -0300

    upgrade: adding middleware to current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::API
   before_action :valid_authenticate
+  before_action :current
+
   include Auths
 
   def valid_authenticate
     authenticate
   end
+
+  def current
+    @usuario = Usuario.find(current_usuario.id)
+  end
+
 end
