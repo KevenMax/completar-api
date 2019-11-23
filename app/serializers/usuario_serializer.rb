d5e0af77d925811982e3dc1901a07e220d125c6d
Keven <kmax136@gmail.com>
2019-11-22 23:20:36 -0300

    upgrade: adding method to user

--- app/serializers/usuario_serializer.rb
+++ app/serializers/usuario_serializer.rb
@@ -1,5 +1,17 @@
 class UsuarioSerializer < ActiveModel::Serializer
-  attributes :id, :email, :nome, :apelido, :matricula, :foto, :contato, :administrador
+  attributes :id, :email, :nome, :apelido, :matricula, :foto, :contato, :administrador, :horas_realizadas, :total_horas, :horas_realizadas
   belongs_to :campu
   belongs_to :curso
+
+  def foto
+    object.foto.url(:big)
+  end
+
+  def horas_realizadas
+    object.horas_realizadas
+  end
+
+  def total_horas
+    object.total_horas
+  end
 end
