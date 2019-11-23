6e43a6b2c420dbfd7d736bd4197fcb8e27815fb7
Keven <kmax136@gmail.com>
2019-11-22 23:16:54 -0300

    upgrade: adding message error in create auth

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -3,18 +3,20 @@ class UsuariosController <  ActionController::API
   include Auths
 
   def create
-    @usuario = Usuario.new(usuarios_params)
+    @usuario = Usuario.new(usuario_params)
     if @usuario.save
       render json: @usuario, status: 201
     else
-      render json: @usuario.errors, status: 422
+      render json: @usuario.errors.full_messages, status: 422
     end
   end
   
   def update
     usuario = Usuario.find(current_usuario.id)
+    curso = Curso.find(usuario_params[:curso_id])
     if usuario.update(usuario_params)
-      render json: usuario, status: 200
+      usuario.update(campu_id: curso.campu_id)
+      render json: usuario, include: [:campu, :curso], status: 200
     else
       render json: usuario.errors.full_messages, status: 422
     end
