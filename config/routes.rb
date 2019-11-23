a43335f26fc77d9285dcc73608f88c31e4fa4c84
Keven <kmax136@gmail.com>
2019-11-22 23:21:18 -0300

    upgrade: adding new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :horas_complementares
   resources :atividades
-  resources :categorias
+  resources :categorias do
+    collection do
+      get 'home'
+    end
+  end
   resources :cursos
   resources :campus
   resources :usuarios, only: [:create] do
