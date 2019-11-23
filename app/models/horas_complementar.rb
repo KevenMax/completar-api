ef46e0b3bb4e70d980d5e1ee0a0e926887304b32
Keven <kmax136@gmail.com>
2019-11-22 23:19:10 -0300

    upgrade: adding field anexo and method

--- app/models/horas_complementar.rb
+++ app/models/horas_complementar.rb
@@ -2,4 +2,11 @@ class HorasComplementar < ApplicationRecord
   belongs_to :categoria
   belongs_to :atividade
   belongs_to :usuario
+
+  mount_uploader :anexo, AnexoUploader
+
+  def quantidade_horas_valido
+    horas_realizadas = HorasComplementar.where(ativo: true, usuario_id: usuario_id, categoria_id: categoria.id).sum('quantidade_horas')
+    return (horas_realizadas + quantidade_horas <= categoria.limite_carga_horaria) ? true : false 
+  end
 end
